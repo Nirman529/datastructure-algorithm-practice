523794d6d4581b06dc67497992facf6dc80b4c3d
Nirman529 <68377229+Nirman529@users.noreply.github.com>
2023-05-08 17:08:09 +0530

    stack completed

--- 1stack/stack.cpp
+++ 1stack/stack.cpp
@@ -1,27 +1,139 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Stack
+{
+    int *arr;
+    int top;
+    int SIZE;
+
+public:
+    Stack(int SIZE)
+    {
+        this->SIZE = SIZE;
+        arr = new int[SIZE];
+        top = -1;
+    }
+
+    bool isFull()
+    {
+        return top == SIZE - 1;
+    }
+
+    bool isEmpty()
+    {
+        return top == -1;
+    }
+
+    void push(int data)
+    {
+        // edge case implementation remaining
+        if (isFull())
+        {
+            cout << "Stack is overflown.\n";
+            return;
+        }
+        top++;
+        arr[top] = data;
+    }
+
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "The Stack is empty.\n";
+            return;
+        }
+        arr[top] = 0;
+        top--;
+    }
+
+    int peep(int i)
+    {
+        if (isEmpty())
+        {
+            cout << "No elements in the stack.\n";
+            return -1;
+        }
+        return arr[i - 1];
+    }
+
+    void change(int pos, int val)
+    {
+        arr[pos] = val;
+    }
+
+    void displayStack()
+    {
+        for (int i = top; i >= 0; i--)
+        {
+            cout << "--> " << arr[i] << endl;
+        }
+        cout << endl;
+    }
+};
+
 int main()
 {
-    int stack_size = 0;
+    int stack_size, opt, data, val;
 
-    cout << "Enter the size of stack: ";
+    cout << "enter stack size:";
     cin >> stack_size;
 
-    vector<int> my_stack;
-    int temp;
-    for (int i = 0; i < stack_size; i++)
-    {
-        cout << "Enter " << i + 1 << "th"
-             << " value in the stack: ";
-        cin >> temp;
-        my_stack.push_back(temp);
-    }
+    Stack s(stack_size);
 
-    for (int i = 0; i < stack_size; i++)
+    do
     {
-        cout << my_stack[i] << endl;
-    }
+        cout << "Enter choice from the following: \n"
+             << "\t1. push\n"
+             << "\t2. pop\n"
+             << "\t3. peep\n"
+             << "\t4. change\n"
+             << "\t5. display stack\n"
+             << "\t6. exit.\nchoice: ";
+        cin >> opt;
+
+        switch (opt)
+        {
+        case 1:
+            cout << "Enter data to push: ";
+            cin >> data;
+            s.push(data);
+            break;
+
+        case 2:
+            s.pop();
+            cout << "Element popped." << endl;
+            break;
+
+        case 3:
+            cout << "Enter element to peep: ";
+            cin >> val;
+            cout << "Element is: " << s.peep(val) << endl;
+            break;
+
+        case 4:
+            cout << "Enter position to change: ";
+            int pos;
+            cin >> pos;
+            cout << "Enter value: ";
+            int val;
+            cin >> val;
+            s.change(pos, val);
+            cout << "Done \n";
+
+            break;
+
+        case 5:
+            cout << "Displaying the stack: \n";
+            s.displayStack();
+            break;
+
+        default:
+            cout << "Exiting operation.";
+            break;
+        }
+    } while (opt < 6);
 
     return 0;
 }
